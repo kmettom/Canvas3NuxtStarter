51196c461977d7069a8bb971ac3514f7b0cffea1
kmettom <kmettom@gmail.com>
2025-04-05 03:07:09 +0200

    Examples

--- pages/index/SectionExamples.vue
+++ pages/index/SectionExamples.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="works-section">
+  <div class="">
     <Container>
       <h2 class="heading-1 text-align-right">
         <span
@@ -20,21 +20,65 @@
           </p>
           <div
             v-onScrollActivate="{ activeRange: 0.9 }"
+            class="example-1"
             @mouseenter="example1Hover = true"
             @mouseleave="example1Hover = false"
           >
-            <h3 class="body-l">Example 1 - Animate item in</h3>
-            <p class="body-xs">
-              All CanvasImage or CanvasText get activated automatically
-            </p>
-            <CanvasImage
-              :src-link="'images/01.JPG'"
-              :uniforms="{
-                uHover: { value: example1Hover ? 1 : 0, duration: 0.55 },
-              }"
-              :load-strategy="'eager'"
-              alt=""
-            />
+            <h3 class="body-l">Basic</h3>
+            <!--            <p class="body-xs">-->
+            <!--              All CanvasImage or CanvasText get activated automatically-->
+            <!--            </p>-->
+            <div class="code-example-wrapper">
+              <CanvasImage
+                :src-link="'images/01.JPG'"
+                :uniforms="{
+                  uHover: { value: example1Hover ? 1 : 0, duration: 0.55 },
+                }"
+                :load-strategy="'eager'"
+                alt=""
+              />
+              <div class="code-container">
+                <pre>
+                  <code>
+                  {
+                    activeRange: 0.99,
+                    activateOnce: true
+                  }
+                  </code>
+                </pre>
+              </div>
+            </div>
+          </div>
+          <div
+            v-onScrollActivate="{ activeRange: 0.9 }"
+            class="example-2"
+            @mouseenter="example1Hover = true"
+            @mouseleave="example1Hover = false"
+          >
+            <h3 class="body-l">Scroll speed</h3>
+            <!--            <p class="body-xs">-->
+            <!--              All CanvasImage or CanvasText get activated automatically-->
+            <!--            </p>-->
+            <div class="code-example-wrapper">
+              <CanvasImage
+                :src-link="'images/01.JPG'"
+                :uniforms="{
+                  uHover: { value: example1Hover ? 1 : 0, duration: 0.55 },
+                }"
+                :load-strategy="'eager'"
+                alt=""
+              />
+              <div class="code-container">
+                <pre>
+                  <code>
+                  {
+                    activeRange: 0.99,
+                    activateOnce: true
+                  }
+                  </code>
+                </pre>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -48,4 +92,11 @@ import Container from "~/components/common/Container.vue";
 const example1Hover = ref(false);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.code-example-wrapper {
+  display: flex;
+}
+.code-container {
+  background: black;
+}
+</style>
