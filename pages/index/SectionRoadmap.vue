97eb98cec2e1238ed96d5063f882a51bddd79910
kmettom <kmettom@gmail.com>
2025-12-30 13:40:33 +0100

    Roadmap update

--- pages/index/SectionRoadmap.vue
+++ pages/index/SectionRoadmap.vue
@@ -4,22 +4,23 @@
       <h2 class="body-s about-headline">Road map</h2>
       <div class="body-l">
         <p
-          v-action-on-scroll="{
+            v-action-on-scroll="{
             activeRange: 0.85,
             activateOnce: true,
             activateCallback: textAniCallback,
           }"
-          class="roadmap-txt"
+            class="roadmap-txt"
         >
-          → Canvas3Text component dynamic colors<br />
-          → 3D model imports<br />
-          → Canvas initiation improvements<br />
+          → Canvas initiation improvements<br/>
+          → 3D model imports<br/>
           <!--          → Responsiveness management - improve Display store<br />-->
-          → Enable/Disable canvas animations dynamically<br />
+          → Enable/Disable canvas animations dynamically<br/>
           <!--          → Side scroll sections<br />-->
-          → Create Type for working with TypeScript<br />
-          → Create SDK for generic usage<br />
-          → Refactor to Typescript <br />
+          → Create Type for working with TypeScript<br/>
+          → Canvas3Text MSDF font implement<br/>
+
+          <!--          → Create SDK for generic usage<br />-->
+          <!--          → Refactor to Typescript <br/>-->
         </p>
       </div>
     </Container>
@@ -29,8 +30,8 @@
 <script setup>
 import Container from "~/components/common/Container.vue";
 
-import { gsap } from "gsap";
-import { SplitText } from "gsap/SplitText";
+import {gsap} from "gsap";
+import {SplitText} from "gsap/SplitText";
 
 gsap.registerPlugin(SplitText);
 
@@ -40,19 +41,19 @@ function textAniCallback(item) {
   const lines = new SplitText(selector, {
     type: "lines",
   }).lines;
-  const wrappedLines = new SplitText(lines, { type: "lines" }).lines;
-  tl.set(wrappedLines, { opacity: 0, y: 50 });
-  tl.set(selector, { opacity: 1, overflow: "hidden" });
-  tl.set(lines, { opacity: 1, overflow: "hidden" });
+  const wrappedLines = new SplitText(lines, {type: "lines"}).lines;
+  tl.set(wrappedLines, {opacity: 0, y: 50});
+  tl.set(selector, {opacity: 1, overflow: "hidden"});
+  tl.set(lines, {opacity: 1, overflow: "hidden"});
   tl.fromTo(
-    wrappedLines,
-    { y: 50, opacity: 1 },
-    {
-      duration: 0.3,
-      opacity: 1,
-      y: 0,
-      stagger: 0.1,
-    },
+      wrappedLines,
+      {y: 50, opacity: 1},
+      {
+        duration: 0.3,
+        opacity: 1,
+        y: 0,
+        stagger: 0.1,
+      },
   );
 }
 </script>
@@ -62,6 +63,7 @@ function textAniCallback(item) {
   margin-left: 20px;
   font-weight: lighter;
 }
+
 .roadmap-section {
   display: grid;
   grid-template-columns: 6fr 14fr;
@@ -73,6 +75,7 @@ function textAniCallback(item) {
     padding: 10vh 10px 125px 10px;
   }
 }
+
 .roadmap-txt {
   line-height: 1.5;
   opacity: 0;
@@ -83,8 +86,10 @@ function textAniCallback(item) {
     margin-top: 10px;
     margin-bottom: 10px;
   }
+
   div {
     opacity: 0;
+
     * {
       opacity: 0;
     }
