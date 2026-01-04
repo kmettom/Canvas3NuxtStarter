3b908294effa874eb3eb8d0dde5ceb736324bf80
kmettom <kmettom@gmail.com>
2026-01-04 16:51:19 +0100

    Nuxt config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
       const css = manifest["node_modules/nuxt/dist/app/entry.js"]?.css;
       if (css) {
         for (let i = css.length - 1; i >= 0; i--) {
-          if (css[i].startsWith("entry")) css.splice(i, 1);
+          if (css[i]?.startsWith("entry")) css.splice(i, 1);
         }
       }
     },
